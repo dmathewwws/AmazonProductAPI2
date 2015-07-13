142e67175ee763d90da2e1d8f820c20b64a7eab8
Daniel Mathews <dannyl.mathews@gmail.com>
2015-07-12 18:34:48 -0700

    pod ready hopefully

---
+++ Example/Pods/Target Support Files/Pods-AmazonProductAPI2_Tests-AmazonProductAPI2/Pods-AmazonProductAPI2_Tests-AmazonProductAPI2-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double AmazonProductAPI2VersionNumber;
+FOUNDATION_EXPORT const unsigned char AmazonProductAPI2VersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-AmazonProductAPI2_Tests/Pods-AmazonProductAPI2_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_AmazonProductAPI2_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_AmazonProductAPI2_TestsVersionString[];
+
